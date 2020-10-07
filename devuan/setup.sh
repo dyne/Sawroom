66d17abb48e0972b3cefcf97a65d645a94cb7896
Jaromil <jaromil@dyne.org>
2020-10-07 16:37:51 +0200

    completed setup script for provisioning

--- devuan/setup.sh
+++ devuan/setup.sh
@@ -10,7 +10,6 @@ print "Installing sawroom on Debian version:"
 ssh $H cat /etc/debian_version
 ssh $H apt-get install -q -y git supervisor daemontools net-tools zsh curl unzip bzip2
 
-
 # docker2sh script
 ssh $H mkdir -p /project
 scp sawroom.debian $H:
@@ -23,3 +22,13 @@ scp supervisord.service $H:/etc/systemd/system/
 ssh $H chmod +x /etc/systemd/system/supervisord.service
 ssh $H systemctl enable supervisord
 
+# sawroom permissions
+chmod a+x /usr/local/bin/*
+mkdir -p /var/log/sawtooth /var/lib/sawroom /var/lib/sawtooth
+chown -R sawroom:sawroom /var/log/sawtooth
+chown -R sawroom:sawroom /var/lib/sawroom
+chown -R sawroom:sawroom /var/lib/sawtooth
+chown -R sawroom:sawroom /etc/sawtooth
+mkdir -p /var/run/tor /var/lib/tor
+chown -R sawroom:sawroom /var/run/tor
+chown -R sawroom:sawroom /var/lib/tor
