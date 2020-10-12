8e6cf6ce93ad0cd8b596df56af72d9696f674dba
Jaromil <jaromil@dyne.org>
2020-10-12 14:26:40 +0200

    small updated to devops docker

--- devops/docker.sh
+++ devops/docker.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-apt-get install \
+apt-get install -y -q \
 		git zsh \
         apt-transport-https \
         ca-certificates \
@@ -11,19 +11,29 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 cat <<EOF > /etc/apt/sources.list.d/docker.list
 deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable
 EOF
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get update -y -q
+apt-get -y -q install docker-ce docker-ce-cli containerd.io
 
 # setup sawroom user
-useradd -ms /bin/zsh sawroom
-usermod -aG docker sawroom
+grep sawroom /etc/passwd
+if [ $? = 1 ]; then
+	useradd -ms /bin/zsh sawroom
+	usermod -aG docker sawroom
+fi
 mkdir -p /home/sawroom/.ssh /var/lib/sawroom
 cp ~/.ssh/authorized_keys /home/sawroom/.ssh/
-chown -R sawroom:sawroom /home/sawroom/.ssh /var/lib/sawroom
+chown -R sawroom:sawroom /var/lib/sawroom
+chsh root -s /bin/zsh
 chsh sawroom -s /bin/zsh
+cat <<EOF > $HOME/.zshrc
+#!/bin/sh
+su - sawroom
+EOF
 cat <<EOF > /home/sawroom/.zshrc
 #!/bin/sh
 cd sawroom && ./shell
 EOF
-git clone https://github.com/dyne/sawroom /home/sawroom/sawroom
-
+if ! [ -r /home/sawroom/sawroom ]; then
+	git clone https://github.com/dyne/sawroom /home/sawroom/sawroom
+fi
+chown -R sawroom:sawroom /home/sawroom
